0321a7f8e31a953410f7b70824ddb2110657f415
Mueni <4missmusau@gmail.com>
2022-08-05 04:28:14 +0300

    updates on tests

--- src/test/java/models/PositionTest.java
+++ src/test/java/models/PositionTest.java
@@ -7,37 +7,37 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PositionTest {
     @Test
     public void NewPositionIsCorrectlyCreated() throws Exception {
-        Position position = setupNewPosition();
+        Position position = NewPosition();
         assertTrue(position instanceof Position);
     }
 
     @Test
     void getNameCorrectly() {
-        Position position = setupNewPosition();
+        Position position = NewPosition();
         assertEquals("CFO", position.getName());
     }
 
     @Test
     void getCreatedCorrectly() {
-        Position position = setupNewPosition();
+        Position position = NewPosition();
         assertEquals("01-01-01", position.getDateCreated());
     }
 
     @Test
     public void PositionWithGetId() throws Exception {
-        Position position = setupNewPosition();
+        Position position = NewPosition();
         position.setId(1);
         assertEquals(1, position.getId());
     }
 
     @Test
     public void PositionWithUpdated() throws Exception {
-        Position position = setupNewPosition();
+        Position position = NewPosition();
         position.setUpdated("01-01-01");
         assertEquals("01-01-01", position.getUpdated());
     }
 
-    public Position setupNewPosition(){
+    public Position NewPosition(){
         return new Position("CFO", "01-01-01");
     }
 }
